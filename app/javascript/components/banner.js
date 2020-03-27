a9b1dc2fcfa61fca96b5eb1aeacf8e7b139c8086
Erin Soggs <erinasoggs@gmail.com>
2020-03-27 15:04:00 -0400

    styled scrolling text

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-text', {
     strings: ["Welcome to Cocktails by Faker"],
-    typeSpeed: 50,
+    typeSpeed: 60,
     loop: true
   });
 }

b6739a426e7d83067cab18feee56308eeda47d65
Erin Soggs <erinasoggs@gmail.com>
2020-04-07 10:56:47 -0400

    making button changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,38 @@ end
 9.times do
   Cocktail.create(name: Faker::FunnyName.name)
 end
+
+# 9.times do
+#   url = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+#   cocktails = open(url).read
+#   cocktails_list = JSON.parse(cocktails)
+#   photo_url = "https://source.unsplash.com/collection/9459762/#{(1..1000).to_a.sample}"
+#   file = URI.open(photo_url)
+#   unless names.include? "#{cocktails_list['drinks'][0]['strDrink']}"
+#     cocktail = Cocktail.new(
+#                   name: "#{cocktails_list['drinks'][0]['strDrink']}",
+#                   instructions: "#{cocktails_list['drinks'][0]['strInstructions']}",
+#                   )
+#     cocktail.photo.attach(io: file, filename: "#{:name}.jpg", content_type: 'image/jpg')
+#     cocktail.save
+#     num = 1
+#     doses = []
+#     until num == 15
+#       ingredient = cocktails_list['drinks'][0]["strIngredient#{num}"]
+#       if ingredient && ingredient != "" && ingredient != "null"
+#         doses << "#{cocktails_list['drinks'][0]["strMeasure#{num}"]}:#{cocktails_list['drinks'][0]["strIngredient#{num}"]}"
+#       end
+#       num += 1
+#     end
+#     doses.each do |d|
+#       a = d.split(':')
+#       dose = Dose.new(description: a.first)
+#       dose.cocktail_id = cocktail.id
+#       if Ingredient.find_by(name: a.last) != nil
+#         ing = Ingredient.find_by(name: a.last)
+#         dose.ingredient_id = ing.id
+#         dose.save
+#       end
+#     end
+#   end
+# end
